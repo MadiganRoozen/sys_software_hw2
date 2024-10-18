8e76a9dd6e084637f451c0523ceb6d1c1cddffa5
alcehmystic <bene.falin@gmail.com>
2024-10-17 23:31:16 -0400

    Token Error Checking Corrected
    Token error checking fixed and working. Filename not working for printing as lexer_main file is not running correctly.

--- hw2-tests/lexer_main.c
+++ hw2-tests/lexer_main.c
@@ -3,3 +3,28 @@
 #include "utilities.h"
 #include "spl.tab.h"
 
+
+//Main Method to Handle Initial Startup From Console
+int main(int argc, char* argv[]) {
+
+	//Checking Args for Filename
+		//Two Args Present - Index 1 is Filename
+		if (argc == 2)
+			lexer_init(argv[1]);
+
+
+	//Running Output
+		lexer_output();
+
+	//Checking For Error Noted
+		if (lexer_has_errors()) {
+			//Errors Noted - Exit 1
+			return 1;
+		}
+		
+		else {
+			//Errors Not Found - Exit 0
+			return 0;
+		}
+}
+
